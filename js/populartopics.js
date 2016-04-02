7ca492514a9f4a7e5d11e26f8803c626adc07d24
Sebastianhadinata <sebastianh.2013@sis.smu.edu.sg>
2016-04-02 17:22:19 +0800

    Completed topics page

--- js/populartopics.js
+++ js/populartopics.js
@@ -1,9 +1,3 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 var myApp = angular.module("myApp", ["firebase"]);
 
 myApp.filter('reverse', function() {
@@ -12,25 +6,8 @@ myApp.filter('reverse', function() {
   };
 });
 
-myApp.controller('topicController', ['$scope', '$firebase',
-    function($scope, $firebase) { 
-    
-        var theFirebaseURL = "https://g1t5-assignment-3.firebaseio.com/";
-        
-        var ref = new Firebase(theFirebaseURL);
-        $scope.topics = $firebase(ref.child("topics").limitToLast(5)).$asArray(); 
-        
-        /*$scope.currentUser = null;
-        //Check to see if the users is already logged in to Firebase and update currentUser if yes. 
-        var authData = ref.getAuth();
-          if (authData && authData !=={}) {
-              $scope.currentUser = $firebase(ref.child("user").child(authData.uid)).$asObject();
-          } else {
-              console.log("User is logged out");
-          }*/
-        
-    
-    }]);
-
-
-
+myApp.controller('topicController', ['$scope', '$firebase', function($scope, $firebase) { 
+  var theFirebaseURL = "https://g1t5-assignment-3.firebaseio.com/";
+  var ref = new Firebase(theFirebaseURL);
+  $scope.topics = $firebase(ref.child("topics").limitToLast(5)).$asArray(); 
+}]);
